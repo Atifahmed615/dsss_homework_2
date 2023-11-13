8ec94d52e0197e5eb0a492c71ef0a06028cd7a92
Atifahmed615 <atifahmed615@gmail.com>
2023-11-14 00:03:51 +0100

    Restructure Project for pip Installability

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your project may have
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.math_quiz:main',
+        ],
+    },
+)
